9e2b7217f20b062a102676b69067adb5f13e3a23
volodialishchyshyn <lishchishin.volodya@gmail.com>
2021-10-18 22:07:34 +0300

    dsada

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,10 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'fsdfs',
   components: {
     HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
